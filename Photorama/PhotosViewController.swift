3062f4592650a121cbe5d6082511e4d63a50a622
jennifertigner <jennifertigner@gmail.com>
2016-03-17 14:48:43 -0700

    Setting up the first view controller

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Jennifer Tigner on 2016-03-17.
+//  Copyright © 2016 Jennifer Tigner. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    
+}
